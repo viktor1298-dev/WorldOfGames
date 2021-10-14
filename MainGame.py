0706c112bc700cab35999d5321e1044749293e29
viktor1298-dev <viktor1298@gmail.com>
2021-10-14 16:49:23 +0300

    test commit + push

--- MainGame.py
+++ MainGame.py
@@ -1,5 +1,6 @@
 from Live import welcome, load_game
-
+from e2e import main_function
 
 welcome()
 load_game()
+main_function()
---
+++ e2e.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+
+
+def test_scores_service():
+    svc = Service(ChromeDriverManager().install())
+    my_driver = webdriver.Chrome(service=svc)
+    my_driver.maximize_window()
+    my_driver.get("http://127.0.0.1:5000/")
+    score = my_driver.find_element(By.ID, "score").text
+    if 1 < int(score) < 1000:
+        return True
+    else:
+        return False
+
+
+def main_function():
+    test_score = test_scores_service()
+    if test_score:
+        exit(0)
+    else:
+        exit(-1)
